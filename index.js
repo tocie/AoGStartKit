9e38805dc4c562110dad6e4803d3d7eaf45c92a0
とし <tocie.co@gmail.com>
2019-07-15 21:48:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const { dialogflow } = require('actions-on-google');
+const {
+    dialogflow,
+    // BasicCard,
+    // BrowseCarousel,
+    // BrowseCarouselItem,
+    // Button,
+    // Carousel,
+    // Image,
+    // LinkOutSuggestion,
+    // List,
+    // MediaObject,
+    // Suggestions,
+    // SimpleResponse,
+    // Table,
+} = require('actions-on-google');
 const app = dialogflow();
-
 
 // For i18n
 const i18n = require('i18n');
@@ -11,7 +24,6 @@ i18n.configure({
     locales:['en', 'ja-JP'],
     directory: __dirname + '/locales'
 });
-
 const moment = require('moment');
 app.middleware((conv) => {
     i18n.setLocale(conv.user.locale);
@@ -21,8 +33,8 @@ const getCurrentDate = moment().format('LL');
 // For i18n END
 
 app.intent('Default Welcome Intent', (conv) => { // must not be async for i18n
-    console.log("getCurrentDate : "+getCurrentDate);      // getCurrentDate : July 15, 2019
-    console.log("conv.user.locale : "+conv.user.locale);  // conv.user.locale : en-US
+    console.log("getCurrentDate : " + getCurrentDate);      // getCurrentDate : July 15, 2019
+    console.log("conv.user.locale : " + conv.user.locale);  // conv.user.locale : en-US
     conv.ask(i18n.__('WELCOME_BASIC', getCurrentDate));
   });
   
