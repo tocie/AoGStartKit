7092cbcd96c8f810d4c058c88ff6e69ef1d7c6a4
とし <tocie.co@gmail.com>
2019-07-15 21:58:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ const {
 } = require('actions-on-google');
 const app = dialogflow();
 
+
 // For i18n
 const i18n = require('i18n');
 i18n.configure({
-    locales:['en', 'ja-JP'],
+    locales: ['en', 'ja-JP'],
     directory: __dirname + '/locales'
 });
 const moment = require('moment');
@@ -29,15 +30,17 @@ app.middleware((conv) => {
     i18n.setLocale(conv.user.locale);
     moment.locale(conv.user.locale);
 });
-const getCurrentDate = moment().format('LL');
 // For i18n END
 
 app.intent('Default Welcome Intent', (conv) => { // must not be async for i18n
-    console.log("getCurrentDate : " + getCurrentDate);      // getCurrentDate : July 15, 2019
-    console.log("conv.user.locale : " + conv.user.locale);  // conv.user.locale : en-US
+    console.log("getCurrentDate : " + getCurrentDate); // getCurrentDate : July 15, 2019
+    console.log("conv.user.locale : " + conv.user.locale); // conv.user.locale : en-US
+
+
+    const getCurrentDate = moment().format('LL');
     conv.ask(i18n.__('WELCOME_BASIC', getCurrentDate));
-  });
-  
+});
+
 app.intent('date', (conv) => { // must not be async for i18n
     conv.ask(i18n.__('DATE', getCurrentDate));
 });
