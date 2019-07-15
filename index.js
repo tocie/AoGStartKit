f56f6766f6ed002ad0bc2628c074fef05adb438f
とし <tocie.co@gmail.com>
2019-07-15 21:41:08 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,26 +8,28 @@ const app = dialogflow();
 // For i18n
 const i18n = require('i18n');
 i18n.configure({
-    locales:['en', 'ja'],
+    locales:['en', 'ja-JP'],
     directory: __dirname + '/locales'
-  });
+});
 
 const moment = require('moment');
 app.middleware((conv) => {
     i18n.setLocale(conv.user.locale);
     moment.locale(conv.user.locale);
 });
+const getCurrentDate = moment().format('LL');
 // For i18n END
 
 app.intent('Default Welcome Intent', (conv) => { // must not be async for i18n
-    conv.ask(i18n.__('WELCOME_BASIC'));
+    console.log("getCurrentDate : "+getCurrentDate);      // getCurrentDate : July 15, 2019
+    console.log("conv.user.locale : "+conv.user.locale);  // conv.user.locale : en-US
+    conv.ask(i18n.__('WELCOME_BASIC', getCurrentDate));
   });
   
 app.intent('date', (conv) => { // must not be async for i18n
-    conv.ask(i18n.__('DATE', moment().format('LL')));
+    conv.ask(i18n.__('DATE', getCurrentDate));
 });
 
 
 //exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
-////exports.<<YOUR_PROJECT>> = functions.https.onRequest(app);
-//exports.test = functions.https.onRequest(app);
+exports.dottdiary = functions.https.onRequest(app);
